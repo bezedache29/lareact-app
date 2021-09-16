e307866379e3c23076b7266d2ced19a123a37657
Christophe Salou <bezedache29@gmail.com>
2021-09-16 12:30:56 +0200

    Composant connaxion

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react'
+import Navbar from '../partials/Navbar'
 
 export class Login extends Component {
   constructor(props) {
     super(props)
   
     this.state = {
-       
+       email: "",
+       pwd: ""
     }
+  }
+
+  handleEmailChange = event => {
+    this.setState({ email: event.target.value }, () => {
+      console.log(this.state)
+    })
+  }
+
+  handlePwdChange = event => {
+    this.setState({ pwd: event.target.value }, () => {
+      console.log(this.state)
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    console.log('connexion')
   }
   
   render() {
     return (
       <>
-        <h1>Page de login</h1>
+        <Navbar />
+
+        <div className="w-50 mx-auto">
+          <h2 className="my-3 text-center">Connexion</h2>
+          <form method="POST" onSubmit={ this.handleSubmit }>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">Adresse email</label>
+              <input type="email" className="form-control" id="email" aria-describedby="emailHelp" onChange={ this.handleEmailChange } />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="pwd" className="form-label">Mot de passe</label>
+              <input type="password" className="form-control" id="pwd" onChange={ this.handlePwdChange } />
+            </div>
+            <button type="submit" className="btn btn-primary">Me connecter</button>
+          </form>
+        </div>
       </>
     )
   }

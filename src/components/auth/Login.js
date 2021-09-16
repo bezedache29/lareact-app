dd80e41e182f9eb6abecc28d244e628b49a5ea0d
Christophe Salou <bezedache29@gmail.com>
2021-09-16 18:44:21 +0200

    Validation des champs front et recupération erreur back + affichage des erreurs + style des inputs (connexion / inscription)

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,4 +1,6 @@
+import axios from 'axios'
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 import Navbar from '../partials/Navbar'
 
 export class Login extends Component {
@@ -7,7 +9,9 @@ export class Login extends Component {
   
     this.state = {
        email: "",
-       pwd: ""
+       password: "",
+       redirect: false,
+       errors: []
     }
   }
 
@@ -17,18 +21,41 @@ export class Login extends Component {
     })
   }
 
-  handlePwdChange = event => {
-    this.setState({ pwd: event.target.value }, () => {
+  handlePasswordChange = event => {
+    this.setState({ password: event.target.value }, () => {
       console.log(this.state)
     })
   }
 
   handleSubmit = event => {
     event.preventDefault()
-    console.log('connexion')
+    
+    let bodyFormData = new FormData()
+    bodyFormData.set('email', this.state.email)
+    bodyFormData.set('password', this.state.password)
+
+    axios.post('http://api.lareact.test/api/login', bodyFormData)
+      .then(res => {
+        console.log(res.data)
+        sessionStorage.setItem('token', res.data.api_token)
+        this.setState({ redirect: true })
+      })
+      .catch(error => {
+        if (error.response.status === 401) {
+          this.setState({ errors: error.response.data.errors }, () => {
+            console.log(this.state.errors)
+          })
+        }
+        console.log(error.response)
+      })
   }
   
   render() {
+
+    if (this.state.redirect) {
+      return <Redirect to="/" />
+    }
+
     return (
       <>
         <Navbar />
@@ -36,15 +63,34 @@ export class Login extends Component {
         <div className="w-50 mx-auto">
           <h2 className="my-3 text-center">Connexion</h2>
           <form method="POST" onSubmit={ this.handleSubmit }>
+          
             <div className="mb-3">
               <label htmlFor="email" className="form-label">Adresse email</label>
-              <input type="email" className="form-control" id="email" aria-describedby="emailHelp" onChange={ this.handleEmailChange } />
+              <input 
+                type="email" 
+                className={ `form-control ${this.state.errors.email ? 'is-invalid' : ''}` } 
+                id="email" 
+                aria-describedby="emailHelp" 
+                onChange={ this.handleEmailChange }
+              />
+              { this.state.errors.email ? <div className="text-danger">{ this.state.errors.email }</div> : '' }
             </div>
+
             <div className="mb-3">
-              <label htmlFor="pwd" className="form-label">Mot de passe</label>
-              <input type="password" className="form-control" id="pwd" onChange={ this.handlePwdChange } />
+              <label htmlFor="password" className="form-label">Mot de passe</label>
+              <input 
+                type="password" 
+                className={ `form-control ${this.state.errors.password ? 'is-invalid' : ''}` }
+                id="password" 
+                onChange={ this.handlePasswordChange }
+              />
+              { this.state.errors.password ? <div className="text-danger">{ this.state.errors.password }</div> : '' }
             </div>
+
+            { this.state.errors.bad_credentials ? <div className="alert alert-warning">{ this.state.errors.bad_credentials }</div> : '' }
+            
             <button type="submit" className="btn btn-primary">Me connecter</button>
+            
           </form>
         </div>
       </>
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -12,7 +12,8 @@ export class Register extends Component {
        email: "",
        password: "",
        password_confirmation: "",
-       redirect: false
+       redirect: false,
+       errors: []
     }
   }
 
@@ -56,7 +57,11 @@ export class Register extends Component {
         this.setState({ redirect: true })
       })
       .catch(error => {
-        console.log(error.response)
+        if (error.response.status === 401) {
+          this.setState({ errors: error.response.data.errors }, () => {
+            console.log(this.state.errors)
+          })
+        }
       })
   }
   
@@ -75,31 +80,47 @@ export class Register extends Component {
           <form method="POST" onSubmit={ this.handleSubmit }>
             <div className="mb-3">
               <label htmlFor="pseudo" className="form-label">Pseudo</label>
-              <input type="text" className="form-control" name="pseudo" id="pseudo" onChange={ this.handlePseudoChange } />
+              <input 
+                type="text" 
+                className={ `form-control ${this.state.errors.pseudo ? 'is-invalid' : ''}` }
+                id="pseudo" 
+                onChange={ this.handlePseudoChange }
+              />
+              { this.state.errors.pseudo ? <div className="text-danger">{ this.state.errors.pseudo }</div> : '' }
             </div>
             <div className="mb-3">
               <label htmlFor="email" className="form-label">Adresse email</label>
-              <input type="email" className="form-control" name="email" id="email" aria-describedby="emailHelp" onChange={ this.handleEmailChange } />
+              <input 
+                type="email" 
+                className={ `form-control ${this.state.errors.email ? 'is-invalid' : ''}` }
+                name="email" 
+                id="email" 
+                aria-describedby="emailHelp" 
+                onChange={ this.handleEmailChange }
+              />
+              { this.state.errors.email ? <div className="text-danger">{ this.state.errors.email }</div> : '' }
             </div>
             <div className="mb-3">
               <label htmlFor="password" className="form-label">Mot de passe</label>
               <input 
                 type="password" 
-                className="form-control" 
+                className={ `form-control ${this.state.errors.password ? 'is-invalid' : ''}` } 
                 name="password" 
                 id="password" 
                 onChange={ this.handlePasswordChange } 
               />
+              { this.state.errors.password ? <div className="text-danger">{ this.state.errors.password }</div> : '' }
             </div>
             <div className="mb-3">
               <label htmlFor="password_confirmation " className="form-label">Confirmation du mot de passe</label>
               <input 
                 type="password" 
-                className="form-control" 
+                className={ `form-control ${this.state.errors.password ? 'is-invalid' : ''}` } 
                 name="password_confirmation" 
                 id="password_confirmation " 
                 onChange={ this.handlePasswordConfirmationChange } 
               />
+              { this.state.errors.password ? <div className="text-danger">{ this.state.errors.password }</div> : '' }
             </div>
             <button type="submit" className="btn btn-primary">M'inscrire</button>
           </form>

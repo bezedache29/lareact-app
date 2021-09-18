005e5517e4b5fc7c959e6a79c9fb416768208c82
Christophe Salou <bezedache29@gmail.com>
2021-09-18 13:44:31 +0200

    Recupération des pictures + affichage selon la recherche

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,12 +9,45 @@ export class Home extends Component {
     super(props)
   
     this.state = {
-       pictures: []
+       pictures: [],
+       search: ""
     }
   }
 
   componentDidMount() {
     axios.get('http://api.lareact.test/api/pictures')
+      .then(res => {
+        this.setState({ pictures: res.data })
+      })
+      .catch(error => {
+        console.log(error.message)
+      })
+  }
+
+  handleSearchChange = event => {
+    this.setState({ search: event.target.value }, () => {
+
+      // On fait les requètes sur l'API a parti de 3 lettres
+      if (this.state.search.length > 3) {
+        this.getArticles()
+      }
+      
+      // Au clic sur la croix dans l'input on recharge les articles
+      if (this.state.search === '') {
+        this.getArticles()
+      }
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+  }
+
+  getArticles = () => {
+    let bodyFormData = new FormData()
+    bodyFormData.set('search', this.state.search)
+
+    axios.post('http://api.lareact.test/api/pictures/search', bodyFormData)
       .then(res => {
         this.setState({ pictures: res.data })
       })
@@ -29,6 +62,17 @@ export class Home extends Component {
         <Navbar />
 
         <div className="container">
+          <div className="d-flex justify-content-center mt-5 mb-3">
+            <form className="form-inline my-2 my-lg-0 mx-2" method="POST" onSubmit={ this.handleSubmit } >
+              <input 
+                type="search" 
+                className="form-control me-sm-2" 
+                name="search" 
+                placeholder="Rechercher un article..." 
+                onChange={ this.handleSearchChange}
+              />
+            </form>
+          </div>
           <div className="row mt-3 justify-content-between">
             {
               this.state.pictures.map(picture => {
